0637d30eddd2e09f802630acceb5c8f912593153
wane228 <56423171+wane228@users.noreply.github.com>
2021-11-28 18:07:51 +1000

    Add files via upload

---
+++ gameclient/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace gameclient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cube1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.cube1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cube1
+            // 
+            this.cube1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.cube1.Location = new System.Drawing.Point(401, 241);
+            this.cube1.Name = "cube1";
+            this.cube1.Size = new System.Drawing.Size(100, 50);
+            this.cube1.TabIndex = 0;
+            this.cube1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(686, 428);
+            this.Controls.Add(this.cube1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.cube1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox cube1;
+        
+    }
+}
+
---
+++ gameclient/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gameclient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyDown += new KeyEventHandler(Move1);
+        }
+
+        void Menu1()
+        {
+           
+        }
+        void Move1(object sendler, KeyEventArgs a)
+        {
+            switch (a.KeyCode.ToString())
+            {
+                case "Left": cube1.Location = new Point(cube1.Location.X - 40, cube1.Location.Y); break;
+                case "Right": cube1.Location = new Point(cube1.Location.X + 40, cube1.Location.Y); break;
+                case "Up": cube1.Location = new Point(cube1.Location.X, cube1.Location.Y - 40); break;
+                case "Down": cube1.Location = new Point(cube1.Location.X, cube1.Location.Y + 40); break;
+
+            }
+        }
+
+    }
+}
